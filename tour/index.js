745a3edf36c7ab1d0ba57b4e39a3ae09eb79a883
engl-2311-022-hcf <64058953+engl-2311-022-hcf@users.noreply.github.com>
2020-04-26 17:53:46 -0500

    tour

---
+++ tour/index.js
@@ -0,0 +1,57 @@
+console.log('alakjfa');
+
+var ssLeft = document.getElementById('slideshow-button-left');
+var ssRight = document.getElementById('slideshow-button-right');
+var ssImg = document.getElementById('slideshow-image');
+
+const var images = [
+	"gettyimages-1040504660-170667a.jpg",
+	"gettyimages-1129133338-170667a.jpg",
+	"gettyimages-1132006407-170667a.jpg",
+	"gettyimages-142020538-170667a.jpg",
+	"gettyimages-165768876-170667a.jpg",
+	"gettyimages-639548209-170667a (1).jpg",
+	"gettyimages-843641798-170667a.jpg",
+	"gettyimages-932389952-170667a.jpg",
+	"gettyimages-952552262-170667a.jpg"
+];
+
+const min = 0;
+const max = images.length - 1;
+var mid = 0;
+
+ssLeft.onclick = moveLeft;
+
+ssRight.onclick = moveRight;
+
+function setImage() {
+	ssImg.style.backgroundImage = 'url("res/' + images[mid] +'")';
+}
+
+function disableButton(elem) {
+	elem.style.display = 'none';
+}
+
+function moveLeft() {
+	if (mid > min) {
+		mid -= 1;
+		
+		setImage();
+		
+		if (mid == min) {
+			disableButton(ssLeft);
+		}
+	}
+}
+
+function moveRight() {
+	if (mid < max) {
+		mid += 1;
+		
+		setImage();
+		
+		if (mid == max) {
+			disableButton(ssRight);
+		}
+	}
+}
